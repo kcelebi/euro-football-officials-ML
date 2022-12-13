df64bf64a53d19b062d4e512dcabcd7ddff66945
Kaya Celebi <kayacelebi17@gmail.com>
2022-12-12 19:22:29 -0500

    simplifying preprocessing

---
+++ pysrc/process.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import lxml.etree as etree
+import sqlite3 as sql
+from tqdm.notebook import tqdm
+
+__all__ = ['DB']
+
+class DB:
+
+    '''
+        DB object to make manipulation cleaner. Creates connection and implements function
+        to make access cleaner.
+
+        Args:
+            db_route: File route to db.sqlite file
+    '''
+    def __init__(self, db_route):
+        self.db_route = db_route
+        self.con = self.create_connection(self.db_route)
+        self.match = self.getDF('Match')
+        self.team = self.getDF('Team')
+        self.team_attr = self.getDF('Team_Attributes')
+        self.league = self.getDF('League')
+        self.player = self.getDF('Player')
+
+        self.match_team = self.joinMatchTeamDF()
+
+    
+    '''
+        Creates SQL db connection to db.sqlite file
+        Args:
+            db_route: Route to db.sqlite file
+        Returns:
+            SQL connection, saves to class object
+    '''
+    def create_connection(self, db_route):
+        return sql.connect(db_route)
+        #df_match = pd.read_sql_query('SELECT * FROM Match', con)
+        #df_team = pd.read_sql_query('SELECT * FROM Team', con)
+
+    '''
+        Creates Pandas Dataframe from SQL connection in db.sqlite file from requested name.
+        Args:
+            df_name: Name of table as it appears in sqlite
+        Returns:
+            Pandas dataframe of requested table
+    '''
+    def getDF(self, df_name):
+        return pd.read_sql_query('SELECT * FROM %s' % df_name, self.con)
+
